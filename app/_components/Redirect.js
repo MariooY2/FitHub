b37ba6c46a1836db28f997689fb7dbecc253a5e6
Mario Youssef <marioyoussef936@gmail.com>
2024-06-27 12:00:57 +0300

    reverted changes

--- app/_components/Redirect.js
+++ app/_components/Redirect.js
@@ -3,7 +3,7 @@ import { useRouter } from 'next/navigation'
  function Redirect(session) {
     const router = useRouter()
     if(session?.user){
-        router.push('/dashboard', { scroll: false })
+        router.push('/dashboard')
     }
    
 }
